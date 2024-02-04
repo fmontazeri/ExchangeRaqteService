5ac6b4e235f0f7258d0d13a125cae6c9799cae43
f.montazeri <montazeri.fm@gmail.com>
2024-02-04 14:38:30 +0330

    WIP

--- Tiba.ExchangeRateService.Domain.Tests.Unit/CurrencyTests/ConstructCurrencyTests.cs
+++ Tiba.ExchangeRateService.Domain.Tests.Unit/CurrencyTests/ConstructCurrencyTests.cs
@@ -17,23 +17,12 @@ public class ConstructCurrencyTests : BaseCurrencyTests
     }
 
     [Fact]
-    public void Constructor_Should_Construct_Currency_With_No_TimePeriods_Successfully()
-    {
-        var actual = _builder.Build();
-
-        actual.Symbol.Should().Be(_builder.Symbol);
-        actual.CurrencyRates.Should().HaveCount(0);
-    }
-  
-
-    [Fact]
-    public void Constructor_Should_Construct_Currency_With_One_TimePeriod_Successfully()
+    public void Construct_Should_Create_A_New_Currency_With_A_TimePeriod_Successfully()
     {
         var actual = _builder
             .WithTimePeriod(Days.TODAY, Days.TODAY.AddDays(Days.SOME_DAYS))
             .Build();
         
-        actual.Symbol.Should().Be(_builder.Symbol);
         actual.CurrencyRates.Should().HaveCount(1);
         actual.Should().BeEquivalentTo(_builder);
     }
@@ -54,7 +43,7 @@ public class ConstructCurrencyTests : BaseCurrencyTests
     [InlineData(Days.FORTH_DAY, Days.SIXTH_DAY, null, Days.THIRD_DAY)] //[4,6] (null,3] 
     [InlineData(Days.FIRST_DAY, Days.THIRD_DAY, Days.FORTH_DAY, null)] //[1,3] [4,null)
     [InlineData(Days.FORTH_DAY, null, Days.FIRST_DAY, Days.THIRD_DAY)] //[4,null) [1,3] 
-    public void Constructor_Should_Construct_Currency_When_There_Is_No_Overlap_Between_The_Give_Open_Interval_Two_TimePeriods(
+    public void Constructor_Should_Construct_Currency_When_There_Is_No_Overlap_Between_The_Two_Given_Open_Interval_TimePeriods(
             int? fromDate1, int? toDate1, int? fromDate2, int? toDate2)
     {
         var timePeriod = GetTimePeriod(fromDate1, toDate1, fromDate2, toDate2);
@@ -74,7 +63,7 @@ public class ConstructCurrencyTests : BaseCurrencyTests
     [InlineData(Days.FIRST_DAY, Days.THIRD_DAY, null, Days.SIXTH_DAY)] //[1,3] (null,6] 
     [InlineData(Days.FIRST_DAY, Days.THIRD_DAY, Days.THIRD_DAY, null)] //[1,3] [3,null)
     [InlineData(null, null, null, null)]
-    public void Constructor_Should_Construct_Currency_When_There_Is_Overlap_Between_The_Give_Open_Interval_Two_TimePeriods(
+    public void Constructor_Should_Construct_Currency_When_There_Is_Overlap_Between_The_Two_Open_Interval_TimePeriods(
             int? fromDate1, int? toDate1, int? fromDate2, int? toDate2)
     {
         var timePeriod = GetTimePeriod(fromDate1, toDate1, fromDate2, toDate2);
@@ -95,7 +84,7 @@ public class ConstructCurrencyTests : BaseCurrencyTests
     [InlineData(Days.SECOND_DAY, Days.FORTH_DAY, Days.FIFTH_DAY, Days.SEVENTH_DAY)] //[2,4] [5,7]
     [InlineData(Days.THIRD_DAY, Days.FIFTH_DAY, Days.SIXTH_DAY, Days.EIGHTH_DAY)]   //[3,5] [6,8]
     [InlineData(Days.FORTH_DAY, Days.SIXTH_DAY, Days.SEVENTH_DAY, Days.NINTH_DAY)]  //[4,6] [7,9]
-    public void Constructor_Should_Construct_Currency_When_There_Is_No_Overlap_Between_The_Give_Close_Interval_List_of_Two_TimePeriods(
+    public void Construct_A_New_Currency_Should_Be_Done_When_There_Is_No_Overlap_Between_The_Two_Given_Close_Interval_TimePeriods(
             int? fromDate1, int? toDate1, int? fromDate2, int? toDate2)
     {
         var timePeriod = GetTimePeriod(fromDate1, toDate1, fromDate2, toDate2);
@@ -115,7 +104,7 @@ public class ConstructCurrencyTests : BaseCurrencyTests
     [InlineData(Days.SECOND_DAY, Days.FIFTH_DAY, Days.THIRD_DAY, Days.SIXTH_DAY)]   //[2,5] [3,6]
     [InlineData(Days.THIRD_DAY, Days.SIXTH_DAY, Days.FORTH_DAY, Days.SEVENTH_DAY)]  //[3,6] [4,7]
     [InlineData(Days.FORTH_DAY, Days.SEVENTH_DAY, Days.FIFTH_DAY, Days.EIGHTH_DAY)] //[4,7] [5,8]
-    public void Constructor_Should_Construct_Currency_When_There_Is_Overlap_Between_The_Give_Close_Interval_List_of_Two_TimePeriods(
+    public void Construct_A_New_Currency_Should_Not_Be_Done_When_There_Is_Overlap_Between_The_Two_Given_Close_Interval_TimePeriods(
             int? fromDate1, int? toDate1, int? fromDate2, int? toDate2)
     {
         var timePeriod = GetTimePeriod(fromDate1, toDate1, fromDate2, toDate2);
